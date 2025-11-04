504a4383d0587cc69b52ea41afbd94a0370ee73a
Sambhav (Sambhav) <zzsa168@gmail.com>
2025-11-04 07:51:03 +0000

    buket is not setup in firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9760454790-8d86a",
   "appId": "1:219858033057:web:5ab826dfe1c86b5495d3e0",
+  "storageBucket": "studio-9760454790-8d86a.appspot.com",
   "apiKey": "AIzaSyBDraiV3YNjz6V1n-dAMhiBhgPV4WR6Lu4",
   "authDomain": "studio-9760454790-8d86a.firebaseapp.com",
   "measurementId": "",
